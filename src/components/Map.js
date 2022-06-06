c2a6e897aa4fd343df31f790da3e9f15cc3842f1
brianveebee <brianbronzini@gmail.com>
2022-06-06 11:04:09 -0700

    Add Map dependencies, use hooks, and set default properties

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,27 @@
-import React from 'react'
+// Enable use hooks
+import React, {useState, useRef, useEffect} from "react";
+// Import dependencies
+import GoogleMapReact from "google-map-react";
+import useSupercluster from "use-supercluster";
 
-function Map = () {
+function Map({center, eventData}) {
+
+// Set zoom use state
+const [zoom, setZoom] = useState(1);
+
   return (
     <div>
       
     </div>
   );
+}
+
+// Set default properties for Map
+Map.defaultProps = {
+  center: {
+    lat: 37.7749,
+    lng: 122.4194
+  }
 }
 
 export default Map;
