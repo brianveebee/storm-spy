ea8c16b115c5d30dcb03acd801d8c23b5f57d442
brianveebee <brianbronzini@gmail.com>
2022-06-15 17:53:16 -0700

    Create Header and Marker components and add icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Map from "./components/Map";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Map />
     </div>
   );
---
+++ src/components/Marker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+
+function Marker({ onClick, id }) {
+  let renderIcon = null
+  
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Marker;
