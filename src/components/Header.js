a8cecb61f6d30ed147cf4cb81186f943a6046a2e
brianveebee <brianbronzini@gmail.com>
2022-06-25 19:16:41 -0700

    Import emojis

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Icon } from '@iconify/react';
+import React from "react";
+import { Icon } from "@iconify/react";
 
 // Creates header container
 function Header(props) {
   return (
-    <div className='header-bar'>
-      <div className='icon'>
-      Storm Spy! <Icon icon= 'emojione:magnifying-glass-tilted-right' />
+    <div className="header-bar">
+      <div className="icon">
+      Storm Spy! <Icon icon= "emojione:magnifying-glass-tilted-right" />
       </div>
     </div>
   );
