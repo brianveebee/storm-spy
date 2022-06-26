45334eefb04b67712ec83d58408bc9f129add77c
brianveebee <brianbronzini@gmail.com>
2022-06-25 19:51:30 -0700

    Add base app function for event rendering and load state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import Map from "./components/Map";
 import Header from "./components/Header";
+import react, { useState, useEffect } from "react";
+// Main context
+import { useMainContext } from "./contexts/context";
 
 function App() {
+  // Deconstruct object from context
+  const { setEventData, reRenderMarkers } = useMainContext();
+  const [loading, setLoading] = useState(false);
+
+  // Render event
+  const [renderEvent, setRenderEvent] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoadingState(true);
+
+    }
+  }, [])
+
   return (
     <div>
       <Header />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // Main Context
@@ -11,6 +11,6 @@ ReactDOM.render(
       <App />
     </ContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
